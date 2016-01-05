09b89e8e111e054880bc08a813a1b284c0949013
holleyangyang@163.com <holleyangyang@163.com>
2016-01-05 09:47:34 +0800

    123

--- src/com/function/buzhou/view/BuZhouController.java
+++ src/com/function/buzhou/view/BuZhouController.java
@@ -9,9 +9,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import net.sf.json.JSONArray;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,29 +17,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.common.util.JsonUtil;
 import com.function.buzhou.service.impl.BuzhouServiceImpl;
-import com.yy.util.ExtHelper;
-
+ 
 @Controller
 public class BuZhouController {
 	
 	@Autowired
 	BuzhouServiceImpl buzhou;
-	
+	/**
+	 * 获取功能对应步骤列表
+	 * @param functionId
+	 * @param request
+	 * @param response
+	 */
 	@RequestMapping(value = "/buzhou/list/{functionId}",method=RequestMethod.POST)
-	public void test1(@PathVariable("functionId") String functionId,HttpServletRequest request,
+	public void list(@PathVariable("functionId") String functionId,HttpServletRequest request,
 			HttpServletResponse response){
-	 
 			Map<String,String> map= new HashMap();
 			map.put("functionId", functionId);
-			
 			String str=buzhou.getList(map);
 			JsonUtil.printJsonListString(request,response,str);
-		 
-	 
    }
-	
-	public void finalize() throws Throwable{
-		super.finalize(); 
-	}
-	 
 }
